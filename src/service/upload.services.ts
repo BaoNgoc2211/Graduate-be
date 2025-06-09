b767fee98be97f202489c350bf5865897328d01e
milopq23 <nhpquy230104@gmail.com>
2025-06-09 23:38:33 +0700

    upload prescription

--- src/service/upload.services.ts
+++ src/service/upload.services.ts
@@ -1,5 +1,12 @@
 import { v2 as cloudinary } from "cloudinary";
 import { IUpload } from "../interface/upload.interface";
+import Tesseract from "tesseract.js";
+
+export interface IMedicienPrescription{
+  medicineName: string,
+  quantity:  number,
+  unit: string,
+}
 
 class UploadServices implements IUpload {
   async uploadSingle(image: string) {
@@ -16,7 +23,35 @@ class UploadServices implements IUpload {
     );
     return result.map((rs) => rs.secure_url);
   }
+  async uploadPrescription(image: string): Promise<IMedicienPrescription[]> {
+    const ocrResult = await Tesseract.recognize(image, 'vie');
+    const rawText = ocrResult.data.text;
+    return this.extractMedicines(rawText);
+  }
+
+  private extractMedicines(text: string): IMedicienPrescription[] {
+    const lines = text.split('\n').filter(line => line.trim() !== '');
+    const result: IMedicienPrescription[] = [];
+    // console.log('Extracted lines:', lines);
+    console.log('Raw text:', text);
+    for (const line of lines) {
+      // console.log('Line:', line);
+      // const match = line.match(/^\d+\.\s*(.+?)\s+(\d+)\s*(viên|gói|chai|ống)?/i);
+      const match = line.match(/^(?:\d+\.\s*)?(.+?)\s+(\d+)\s*(viên|gói|chai|ống)?/i);
+      if (match) {
+        result.push({
+          medicineName: match[1].trim(),
+          quantity: Number(match[2]),
+          unit: match[3]?.toLowerCase() || '',
+        });
+      }
+      // console.log(medicineName, quantity, unit);,
+    }
+    
+    return result;
+  }
 }
+
 
 const uploadService = new UploadServices();
 export default uploadService;
